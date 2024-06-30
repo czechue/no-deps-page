19dac30235317ca4e3cfaf6e088c986faef1922b
Michal <michal.lester@fourthwall.com>
2024-06-30 11:20:47 +0200

    fix duplicated path names

--- public/routing/routing.js
+++ public/routing/routing.js
@@ -10,9 +10,23 @@ export class HashRouter {
       link.addEventListener("click", (event) => {
         event.preventDefault();
         const href = event.target.href;
-        const pathname = new URL(href).pathname;
+        const { pathname: linkPathname } = new URL(href);
+        const {
+          location: { pathname },
+        } = window;
 
-        window.location.hash = `#${pathname}`;
+        // linkPathname: "/no-deps-page/public/page-2"
+        // pathname: "/no-deps-page/public/"
+        // hash: "#/no-deps-page/public/page-2"
+
+        let dedupedLinkPathname = linkPathname;
+
+        if (pathname !== "/") {
+          const pathnameWithoutSlash = pathname.slice(0, -1); // "/no-deps-page/public"
+          dedupedLinkPathname = linkPathname.replace(pathnameWithoutSlash, "");
+        }
+
+        window.location.hash = `#${dedupedLinkPathname}`;
       });
     });
   }
